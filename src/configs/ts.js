daa0b33af4962c084bcf0321d2d3a28e19feda8d
lai9fox <1591765125@qq.com>
2025-05-15 19:56:05 +0800

    fix: ts no-unused-vars 错误

--- src/configs/ts.js
+++ src/configs/ts.js
@@ -4,4 +4,10 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   tseslint.configs.recommended,
   jsConfig,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
 );
